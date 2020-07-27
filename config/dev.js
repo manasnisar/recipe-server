9611e24624965c1169c534f1738d16173b831dcd
manasnisar <anasnisar980@gmail.com>
2020-07-27 22:58:25 +0500

    final

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    ATLAS_URI : "mongodb+srv://anasnisar:letmeintom0ng0db@click-app-demo.hcqci.gcp.mongodb.net/click-app-demo?retryWrites=true&w=majority"
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/key')
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(config.ATLAS_URI,{useUnifiedTopology:true,useCreateIndex:true,useNewUrlParser:true});
+
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Connection to MongoDB database has been successfully established.")
+}).catch(err => console.error(err));
+
+
+
+const userRouter = require('./routes/users');
+const favouriteRouter = require('./routes/favourites')
+
+app.use('/api/user', userRouter);
+app.use('/api/favourites' , favouriteRouter)
+
+// app.get('/',(req,res) => {
+//     res.json({"hello" : "I am deployed on heroku."})
+// })
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>{
+    console.log(`Server is listening at port ${port}`)
+});
